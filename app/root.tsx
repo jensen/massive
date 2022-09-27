450ae0e1faf6e63e94ab8827a7bcb0e5f3ffb88a
Karl Jensen <karljensen@gmail.com>
2022-09-27 15:18:30 -0700

    Give application a new title

--- app/root.tsx
+++ app/root.tsx
@@ -17,7 +17,7 @@ export function links() {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Massive Uploader",
   viewport: "width=device-width,initial-scale=1",
 });
 
